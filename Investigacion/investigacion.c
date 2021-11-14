c58e9cd9484bf2cf3907a318d0be01d51560a731
Ignacio-Viquez <joseignacioviquezrojas@gmail.com>
2021-11-14 15:17:49 -0600

    Evaluacion de soluciones y ciclo principal de la metaheuristica

--- Investigacion/investigacion.c
+++ Investigacion/investigacion.c
@@ -10,6 +10,9 @@ int  fila = 3;
 int  columna = 6;
 int seed = 10395922; // Numero aleatorio para los numeros generados 
 int rango_velocidad = 12; // Rango para la velocidad
+int cantidad_iteraciones = 500;
+int iteraciones_mejor_solucion = 0;
+
 
 int menor_X_global = 99999;
 int menor_Y_global = 99999;
@@ -66,8 +69,9 @@ int main(int argc, char const *argv[])
 		matris[i][5] = numero_aleatorio(rango_velocidad);
 	}
 
-	//Iniciar el valor de G con los valores anteriormente declarados
-	for (int i = 0; i < fila; ++i){
+	//Iniciar el valor de G con los valores anteriormente declarados --OJO! 
+	for (int i = 0; i < fila; ++i)
+	{
 		double evaluacion = calcular_funcion(matris[i][0],matris[i][1]);
 		printf("La evaluacion para %d dio: %f\n",i, evaluacion );
 		if (evaluacion < menor_global){
@@ -76,6 +80,49 @@ int main(int argc, char const *argv[])
 			menor_Y_global = matris[i][1];
 		}
 	}
+
+	//Ciclo principal del algoritmo
+	while((cantidad_iteraciones != 0) ){
+		printf("Iteración: %d\n",cantidad_iteraciones );
+		for (int i = 0; i < fila; i++)
+		{
+			//Calcular la función para la posicion actual de la particula i
+			double evaluacion = calcular_funcion(matris[i][0],matris[i][1]);
+
+			//Comprobacion con la mejor posicion de la particula i 
+			double mejor_eval = calcular_funcion(matris[i][2],matris[i][3]);
+			if(evaluacion < mejor_eval){
+				// Asignamos las coordenadas 
+				matris[i][2] = matris[i][0]; //Coordenada de la mejor posicion de X
+				matris[i][3] = matris[i][1]; //Coordenada de la mejor posicion de Y
+			}
+
+			//Comprobacion con la mejor solucion global si no cambia la solucion global aumentar el contador de iteraciones de la solucion global
+			if(evaluacion < menor_global){
+				double diferencia_evaluaciones = menor_global - evaluacion; 
+				if (0.0001 <= diferencia_evaluaciones){
+					menor_global = evaluacion;
+					menor_X_global = matris[i][0];
+					menor_Y_global = matris[i][1];
+
+					//Ponemos en 0 la iteraciones desde que encontramos esta solucion para contar las 100 
+					iteraciones_mejor_solucion = 0;
+				} 
+
+			}
+			//Ajustar la velocidad y posicion de la particula i
+			//Parte KEVIN
+		}
+
+		iteraciones_mejor_solucion = iteraciones_mejor_solucion + 1;
+		cantidad_iteraciones = cantidad_iteraciones - 1;
+		if (iteraciones_mejor_solucion == 100){
+			// Terminamos la ejecucion
+			cantidad_iteraciones = 0;
+		}
+
+	}
+
 
 	imprimir();
 	return 0;
